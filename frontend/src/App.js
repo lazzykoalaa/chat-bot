80de405a3dccd6a23862d4e4ff8214100d6ad7f8
Gurjot Kaur <112189682+gjyotk@users.noreply.github.com>
2024-08-11 22:10:08 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
+import ChatBox from './components/ChatBox';
 import './App.css';
+import logo from './assets/logo.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <h1>Gemini Chatbot</h1> */}
       </header>
+      <ChatBox />
     </div>
   );
 }
